fcaf6fb3a069ea5399e72d999962f74866b5da89
Michal <michalkry89@hotmail.es>
2015-05-05 22:51:49 +0200

    Servidor

---
+++ WebService/WebService/M13ProjectWcfDataService.svc.cs
@@ -0,0 +1,37 @@
+//------------------------------------------------------------------------------
+// <copyright file="WebDataService.svc.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Data.Services;
+using System.Data.Services.Common;
+using System.Data.Services.Providers;
+using System.Linq;
+using System.ServiceModel.Web;
+using System.Web;
+
+namespace WebService
+{
+    public class M13ProjectWcfDataService : EntityFrameworkDataService<m13_projectEntities>
+    {
+        // This method is called only once to initialize service-wide policies.
+        public static void InitializeService(DataServiceConfiguration config)
+        {
+            config.SetEntitySetAccessRule("Comanda", EntitySetRights.AllRead);
+            config.SetEntitySetAccessRule("Producte", EntitySetRights.All);
+            config.SetEntitySetAccessRule("Usuari", EntitySetRights.All);
+            config.SetEntitySetAccessRule("Comercial", EntitySetRights.All);
+            config.SetEntitySetAccessRule("Administrador", EntitySetRights.All);
+            config.SetEntitySetAccessRule("Localitzacio", EntitySetRights.AllRead);
+            config.SetEntitySetAccessRule("Comanda_Producte", EntitySetRights.AllRead);
+            config.SetEntitySetAccessRule("Client", EntitySetRights.AllRead);
+            config.SetEntitySetAccessRule("Categoria", EntitySetRights.All);
+            config.SetEntitySetAccessRule("ProducteLog", EntitySetRights.AllRead);
+            config.SetEntitySetAccessRule("CategoriaLog", EntitySetRights.AllRead);
+            // config.SetServiceOperationAccessRule("MyServiceOperation", ServiceOperationRights.All);
+            config.DataServiceBehavior.MaxProtocolVersion = DataServiceProtocolVersion.V3;
+        }
+    }
+}
